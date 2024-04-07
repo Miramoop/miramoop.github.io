baa7852e3ae494cf87b2342af2f6b5a371277423
Miranda Morris <juniperistic@gmail.com>
2024-04-07 19:48:08 -0400

    Changes to Links

--- main.js
+++ main.js
@@ -1,27 +1,47 @@
 
-    var tablinks = document.getElementsByClassName("tab-links");
-    var tabcontents = document.getElementsByClassName("tab-contents");
-    
-    function opentab(tabname){
-        for(tablink of tablinks){
-            tablink.classList.remove("active-link");
-        }
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
 
-        for(tabcontent of tabcontents){
-            tabcontent.classList.remove("active-tab");
-        }
-
-        event.currentTarget.classList.add("active-link");
-        document.getElementById(tabname).classList.add("active-tab");
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
     }
 
-    var sidemenu = document.getElementById("sidemenu");
-
-    function openmenu(){
-        sidemenu.style.right = "0";
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
     }
 
-    function closemenu(){
-        sidemenu.style.right = "-200px";
-    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu(){
+    sidemenu.style.right = "0";
+}
+
+function closemenu(){
+    sidemenu.style.right = "-200px";
+}
+
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const sideMenu = document.querySelector('.side-menu');
+const sideMenuItems = Array.from(sideMenu.querySelectorAll('a'));
+
+hamburgerMenu.addEventListener('click', () => {
+    sideMenu.classList.toggle('active');
+});
+
+sideMenuItems.foreach((link) => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
+        sideMenuItems.forEach((item) => {
+            item.classList.add('active');
+        });
+        link.classList.add('active');
+    });
+});
+
+
 
